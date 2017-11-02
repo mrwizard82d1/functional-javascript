f9002ad073ea43e05ec7a0a12f2d760d392463f7
Larry Jones <larryj@blinds.com>
2017-11-02 17:57:08 -0500

    Convert code to ES7

--- first-class.js
+++ first-class.js
@@ -1,26 +1,31 @@
 // First class functions
 
-var fortyTwo = function() { return 42; }
-console.log('A function stored in a variable: ', fortyTwo);
-console.log('And invoked: ', fortyTwo());
+const fortyTwo = () => 42;
+console.log(`A function stored in a variable: ${fortyTwo}`);
+console.log(`And invoked: ${fortyTwo()}`);
 
-var fortyTwos = [42, function() { return 42; }];
+const fortyTwos = [42, () => 42];
 console.log();
-console.log('A function can be stored in an array: ', fortyTwos);
+console.log(`A function can be stored in an array: ${fortyTwos}`);
 
-var fortyTwoObj = {
+const fortyTwoObj = {
   number: 42,
   fun: function() { return 42; },
 };
 console.log();
-console.log('A function can be stored as a value in an object: ', fortyTwoObj);
+console.log(`A function can be stored as a value in an object: ${fortyTwoObj}`);
 
 console.log();
-console.log('A function can be created (inline) as needed: ', 42 + (function () { return 42; })());
+console.log(`A function can be created (inline) as needed: ${42 + (() => 42)()}`);
 
 function weirdAdd(n, f) {
   return n + f();
 }
 console.log();
-console.log('A number can be passed as an argument just like a function: ',
-  weirdAdd(42, function() { return 42; }));
+console.log(
+  `A number can be passed as an argument just like a function: ${weirdAdd(42, (() => 42))}`);
+
+console.log();
+console.log(`A number can be returned from a function: ${(() => 42)()}`);
+console.log(`And so can a function: ${(() => () => 42)()}`);
+
