3734eba79c0be9c947c93a8a62a5037b798c2c89
Larry Jones <larryj@blinds.com>
2017-11-04 17:35:38 -0500

    Applicativet programming examples
    - _.reduceRight
    - _.find
    - available predicates
    - _.reject
    - complement
    - _.all
    - _.any
    - _.sortBy
    - _.groupBy
    _ _.countBy

--- applicative.js
+++ applicative.js
@@ -45,3 +45,110 @@ console.log();
 console.log('For completeness, _.map takes a third argument: the (complete) collection');
 const completeMapResult = _.map(anObject, (v, k, coll) => [k, v, dumpObject(coll)]);
 console.log(`Using a function of three arguments: ${completeMapResult}`);
+
+console.log();
+console.log('Contrast _.reduce and _.reduceRight');
+
+function div(x, y) {
+  const result =  x / y;
+  console.log(`div(${x}, ${y}) === ${result}`);
+  return result;
+}
+
+console.log(`_.reduce([100, 2, 25], div) === ${_.reduce([100, 2, 25], div)}`);
+console.log(`_.reduceRight([100, 2, 25], div) === ${_.reduceRight([100, 2, 25], div)}`);
+
+console.log();
+console.log('We can use _.reduceRight to build other functions');
+
+function allOf(/* functions */) {
+  return _.reduceRight(arguments, (truth, f) => truth && f(), true);
+}
+
+function anyOf(/* functions */) {
+  return _.reduceRight(arguments, (truth, f) => truth || f(), false);
+}
+
+const T = () => true;
+const F = () => false;
+
+console.log(`allOf() === ${allOf()}`);
+console.log(`allOf(T, T) === ${allOf(T, T)}`);
+console.log(`allOf(T, T, T, T, F) === ${allOf(T, T, T, T, F)}`);
+
+console.log();
+console.log(`anyOf(T, F, F) === ${anyOf(T, F, F)}`);
+console.log(`anyOf(F, F, F, F) === ${anyOf(F, F, F, F)}`);
+console.log(`anyOf() === ${anyOf()}`);
+
+console.log();
+console.log(`_.find(['a', 'b', 3, 'd'], _.isNumber) === ${_.find(['a', 'b', 3, 'd'], _.isNumber)}`);
+
+console.log();
+console.log('Available predicates:\n');
+_.each(['_.isEqual', '_.isEmpty', '_.isElement', '_.isArray', '_.isObject', '_.isArguments',
+        '_.isFunction', '_.isString', '_.isNumber', '_.isFinite', '_.isBoolean', '_.isDate,',
+        '_.isRegExp', ' _.isNan', '_.isNull', '_.isUndefined'], p => console.log(p));
+
+console.log();
+console.log(
+  `_.reject(['a', 'b', 3, 'd'], _.isNumber) === ${_.reject(['a', 'b', 3, 'd'], _.isNumber)}`);
+
+/**
+ * Complements the application of 'pred`
+ *
+ * @param pred The predicate to coplemerct
+ * @returns A function that, when invoked, calculates the logical negation of applying `pred` to
+ * the function arguments.
+ */
+function complement(pred) {
+  return function(/* arguments */) {
+    return !pred.apply(null, _.toArray(arguments));
+  }
+}
+
+const filterComplementResult = _.filter(['a', 'b', 3, 'd'], complement(_.isNumber));
+console.log();
+console.log(`_.filter(["a", "b", 3, "d"], complement(_.isNumber)) === ${filterComplementResult}`);
+
+console.log();
+console.log(`_.all([1, 2, 3, 4], _.isNumber) === ${_.all([1, 2, 3, 4], _.isNumber)}`);
+console.log(`_.all([1, 2, 3, 4, F()], _.isNumber) === ${_.all([1, 2, 3, 4, F()], _.isNumber)}`);
+
+console.log();
+console.log(`_.any([1, 2, 'c', 4], _.isString) === ${_.any([1, 2, 'c', 4], _.isString)}`);
+console.log(`_.any([1, 2, 3, 4], _.isString) === ${_.any([1, 2, 3, 4], _.isString)}`);
+
+function dumpArray(a) {
+  return _.each(a, i => dumpObject(i));
+}
+
+const people = [{name: 'Rick', age: 30}, {name: 'Jaka', age: 24}];
+console.log();
+console.log('people ===', dumpArray(people));
+
+const peopleByAge = _.sortBy(people, p => p.age);
+console.log('_.sortBy(people, p => p.age) === ', dumpArray(peopleByAge));
+
+const albums = [
+  {
+    title: 'Sabbath Bloody Sabbath',
+    genre: 'Metal',
+  },
+  {
+    title: 'Scientist',
+    genre: 'Dub',
+  },
+  {
+    title: 'Undertow',
+    genre: 'Metal',
+  },
+];
+const albumsByGenre = _.groupBy(albums, a => a.genre);
+
+console.log();
+console.log('albums = ', albums);
+console.log('_.groupBy(albums, a => a.genre) ===', albumsByGenre);
+
+console.log();
+console.log('_.countBy(albums, a => a.genre ===', _.countBy(albums, a => a.genre));
